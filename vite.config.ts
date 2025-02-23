d2c74abf92bc706ad17cb50b0589033e5a3c82d9
Bifin Babu <bifinbabu088@gmail.com>
2025-02-24 01:06:28 +0530

    feat: update vite config for prod build

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  build: {
+    watch: null,
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      
     },
   },
-})
+});
