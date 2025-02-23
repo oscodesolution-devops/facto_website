fb974047e48bc3da269eaf9fa990f4f701217369
Bifin Babu <bifinbabu088@gmail.com>
2025-02-24 01:07:09 +0530

    fix: update base url

--- src/utils/apiConstants.ts
+++ src/utils/apiConstants.ts
@@ -1,6 +1,7 @@
 // export const BASE_URL = 'https://facto.org.in/api/v1';
-export const BASE_URL = "http://localhost:3000/api/v1";
-// export const BASE_URL = 'https://facto.org.in/api/v1';
+// export const BASE_URL = "http://localhost:3000/api/v1";
+// export const BASE_URL = "http://13.202.108.149/api/v1";
+export const BASE_URL = "https://api.facto.org.in/api/v1";
 export const API_CONSTANTS = {
   PROFILE: "/profile",
 };
