e9068ccab1edaa51ec82dea61db309687e93ea81
Bifin Babu <bifinbabu088@gmail.com>
2025-03-03 01:38:23 +0530

    feat: Add local testing base url

--- src/utils/apiConstants.ts
+++ src/utils/apiConstants.ts
@@ -2,6 +2,7 @@
 // export const BASE_URL = "http://localhost:3000/api/v1";
 // export const BASE_URL = "http://13.202.108.149/api/v1";
 export const BASE_URL = "https://api.facto.org.in/api/v1";
+// export const BASE_URL = "http://localhost:8080/api/v1";
 export const API_CONSTANTS = {
   PROFILE: "/profile",
 };
